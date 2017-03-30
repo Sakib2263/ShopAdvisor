bd624e1d791f3881b353f83be4f573f38254b2f5
Tonmoy <thTonmoy@users.noreply.github.com>
2017-03-30 23:07:01 +0600

    minor fixes

--- ShopAdvisor/src/ui/LoginController.java
+++ ShopAdvisor/src/ui/LoginController.java
@@ -1,16 +1,7 @@
 package ui;
 
-import java.util.Calendar;
 import java.net.URL;
 import java.util.ResourceBundle;
-<<<<<<< HEAD
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
- 
-=======
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -22,20 +13,8 @@ import javafx.scene.control.TextField;
  * FXML Controller class
  *
  */
->>>>>>> origin/master
 public class LoginController implements Initializable {
-    ObservableList<String>SigninList = FXCollections.observableArrayList("Buyer","Seller");
-    @FXML
-    private ChoiceBox SigninBox;
-    
-    @FXML
-    public void initialize() {
-        SigninBox.setValue("Buyer");
-        SigninBox.setItems(SigninList);
-    }     
 
-<<<<<<< HEAD
-=======
     @FXML
     private Button signinButton;
     @FXML
@@ -50,10 +29,9 @@ public class LoginController implements Initializable {
     /**
      * Initializes the controller class.
      */
->>>>>>> origin/master
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
     
 }
