12bfdb592191aee6f0c671322048189b977f9158
Tonmoy <thTonmoy@users.noreply.github.com>
2017-04-07 21:56:37 +0600

    bug fixes, separate styling for 2 ui

--- ShopAdvisor/src/ui/SignupController.java
+++ ShopAdvisor/src/ui/SignupController.java
@@ -11,12 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 /**
