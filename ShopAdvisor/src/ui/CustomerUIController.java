bb71165225455e08908f2f2d09e7e3c87b655bac
Sakib2263 <sakibhasan2263@gmail.com>
2017-04-11 23:26:57 +0600

    all products complete for now
    will be working on new idea soon

--- ShopAdvisor/src/ui/CustomerUIController.java
+++ ShopAdvisor/src/ui/CustomerUIController.java
@@ -63,6 +63,30 @@ public class CustomerUIController implements Initializable {
     @FXML
     private TableColumn<Product, Double> priceColumn14;
     @FXML
+    private TableView<Product> table111;
+    @FXML
+    private TableColumn<Product, String> storeColumn111;
+    @FXML
+    private TableColumn<Product, Double> priceColumn111;
+    @FXML
+    private TableView<Product> table121;
+    @FXML
+    private TableColumn<Product, String> storeColumn121;
+    @FXML
+    private TableColumn<Product, Double> priceColumn121;
+    @FXML
+    private TableView<Product> table131;
+    @FXML
+    private TableColumn<Product, String> storeColumn131;
+    @FXML
+    private TableColumn<Product, Double> priceColumn131;
+    @FXML
+    private TableView<Product> table141;
+    @FXML
+    private TableColumn<Product, String> storeColumn141;
+    @FXML
+    private TableColumn<Product, Double> priceColumn141;
+    @FXML
     private TableView<Product> wishlistTable; ////// whish list
     @FXML
     private TableColumn<Product, String> product_wishlist;
@@ -78,6 +102,7 @@ public class CustomerUIController implements Initializable {
     private AnchorPane rightPane;
     @FXML
     ObservableList<Product> SelectedProducts = FXCollections.observableArrayList();
+    
      @FXML
     void hideWishlist(ActionEvent event) throws IOException {
         rootPane.setRight(null);
@@ -151,7 +176,7 @@ public class CustomerUIController implements Initializable {
         initializeColumns(store_wishlist,price_wishlist);
         product_wishlist.setCellValueFactory(new PropertyValueFactory<>("Name"));
     }
-    @FXML
+        @FXML
     void addToWishListAction11(ActionEvent event) {
         addSelecteditem(table11);
         makewishlist();
@@ -170,23 +195,49 @@ public class CustomerUIController implements Initializable {
     void addToWishListAction14(ActionEvent event) {
         addSelecteditem(table14);
         makewishlist();
+    }@FXML
+    void addToWishListAction111(ActionEvent event) {
+        addSelecteditem(table111);
+        makewishlist();
     }
+        @FXML
+    void addToWishListAction121(ActionEvent event) {
+        addSelecteditem(table121);
+        makewishlist();
+    }
+        @FXML
+    void addToWishListAction131(ActionEvent event) {
+        addSelecteditem(table131);
+        makewishlist();
+    }
+        @FXML
+    void addToWishListAction141(ActionEvent event) {
+        addSelecteditem(table141);
+        makewishlist();
+    }
+
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializePanes();
+        //Category 1
         initializeData(table11,"burger");
-        initializeColumns(storeColumn11,priceColumn11);
-        
+        initializeColumns(storeColumn11,priceColumn11);    
         initializeData(table12,"sandwitch");
-        initializeColumns(storeColumn12,priceColumn12);
-        
+        initializeColumns(storeColumn12,priceColumn12);   
         initializeData(table13,"taco");
         initializeColumns(storeColumn13,priceColumn13);
-        
         initializeData(table14,"fried_chicken");
         initializeColumns(storeColumn14,priceColumn14);
-        
+        //Category 2
+        initializeData(table111,"Pendrives");
+        initializeColumns(storeColumn111,priceColumn111);
+        initializeData(table121,"HDD");
+        initializeColumns(storeColumn121,priceColumn121);
+        initializeData(table131,"Joystick");
+        initializeColumns(storeColumn131,priceColumn131);
+        initializeData(table141,"UPS");
+        initializeColumns(storeColumn141,priceColumn141);   
     }
-
 }
