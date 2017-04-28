1bd4351ad8c9f20f3cf06ad6e81980441fac1087
Sakib2263 <sakibhasan2263@gmail.com>
2017-04-29 00:56:59 +0600

    feedback page changed
    Plz connect one webpage ,, I will do the rest tomorrow :/

--- ShopAdvisor/src/ui/FeedBackController.java
+++ ShopAdvisor/src/ui/FeedBackController.java
@@ -2,20 +2,16 @@ package ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 
 public class FeedBackController implements Initializable {
 
-    @FXML
-    private ChoiceBox sellerChoice;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        sellerChoice.setItems(FXCollections.observableArrayList("KFC", "Subway", "BFC", "Ryans", "Transcend"));
-        sellerChoice.setValue("Subway");
+
     }
 
 }
